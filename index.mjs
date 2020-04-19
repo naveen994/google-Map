60207a01b0c89fa53035bb4ca21d574bd0af4732
naveen994 <62063061+naveen994@users.noreply.github.com>
2020-04-19 22:51:43 +0530

    Add files via upload

---
+++ index.mjs
@@ -0,0 +1,134 @@
+
+
+
+var map;
+var markers = [];
+var infoWindow;
+var locationSelect;
+
+
+
+function initMap() {
+    var sydney = {lat: 34.0522, lng: -118.243683};
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: sydney,
+      zoom: 11,
+      mapTypeId: 'roadmap',
+      
+    });
+    infoWindow = new google.maps.InfoWindow();
+    searchStores();
+
+}
+
+function searchStores() {
+    var foundStores = [];
+    var zipCode = document.getElementById("zip-code-input").value;
+if (zipCode){
+    stores.forEach(function (store,index) {
+        var postal = store.address.postalCode.substring(0,5);
+        if(postal == zipCode){
+            foundStores.push(store);
+        }
+        
+    })
+
+
+}else{
+    foundStores = stores;
+} 
+
+    clearLocations();
+    displayStores(foundStores);
+    showStoreMarkers(foundStores);
+    setOnClickListener()
+}
+
+
+function clearLocations() {
+    infoWindow.close();
+    for (var i = 0; i < markers.length; i++) {
+      markers[i].setMap(null);
+    }
+    markers.length = 0;
+
+  }
+
+function setOnClickListener() {
+    var storesElement = document.querySelectorAll('.store-container');
+    storesElement.forEach(function (elem,index) {
+        elem.addEventListener(`click`,function () {
+            new google.maps.event.trigger(markers[index],`click`);
+            
+        })
+        
+    })
+
+    
+}
+
+function displayStores(stores) {
+
+    var storeHtml = '';
+    stores.forEach(function (store,index) {
+        var address = store.addressLines;
+        var phone = store.phoneNumber;
+
+
+        storeHtml += `<div class="store-container">
+        <div class="store-container-background">
+            <div class="store-info-container">
+                <div class="store-address">
+                    <span>${address[0]}</span>
+                    <span>${address[1]}</span>
+                </div>
+                <div class="store-phone-number">${phone}</div>
+        </div>
+            <div class="store-number-container">
+                <div class="store-number">${index+1}</div>
+
+
+            </div>
+        </div>
+
+
+    </div>`
+        
+    });
+
+    document.querySelector('.stores-list').innerHTML=storeHtml;
+    
+}
+
+function showStoreMarkers(stores) {
+    var bounds = new google.maps.LatLngBounds();
+    stores.forEach(function (store,index) {
+        
+        var latlng = new google.maps.LatLng(
+            store.coordinates.latitude,
+            store.coordinates.longitude);
+
+        var name = store.name;
+        var address = store.addressLines[0];
+        createMarket(latlng,name,address,index)
+        bounds.extend(latlng);
+    });
+    map.fitBounds(bounds);
+
+    
+}
+
+function createMarket(latlng,name,address,index) {
+    var html = "<b>" + name + "</b> <br/>" + address;
+    var marker = new google.maps.Marker({
+      map: map,
+      position: latlng,
+      label: `${index}`
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+      infoWindow.setContent(html);
+      infoWindow.open(map, marker);
+    });
+    markers.push(marker);
+      
+}
